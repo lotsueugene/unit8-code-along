daec605c0b20860cb642cb6bb352a8c59e5fee38
Eugene Lotsu <lotsue.24@gmail.com>
2026-03-14 01:33:18 -0500

    test db connection

--- server.js
+++ server.js
@@ -8,3 +8,20 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Test database connection
+async function testConnection() {
+    try {
+        await db.authenticate();
+        console.log('Connection to database established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+testConnection();
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
